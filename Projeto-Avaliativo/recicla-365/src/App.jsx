974232e31a60f23667befcb62d0e5109dd0e66f0
FernandoBack <fernandoback@gmail.com>
2025-08-29 22:28:37 -0300

    feat: Configura estrutura inicial e página de Login com MUI
    - Integração da biblioteca de componentes Material-UI (MUI) como base
      do nosso Design System, incluindo a instalação de dependências como
      @emotion, a fonte Roboto e os ícones do Material.
    
    - Criação da estrutura visual da página de Login (`Login.jsx`),
      utilizando uma abordagem híbrida de estilização:
      - CSS Modules (`Login.module.css`) é usado para o layout e
        posicionamento geral da página.
      - As props nativas do MUI (`variant`, `margin`, etc.) são usadas para
        a estilização interna dos componentes.
    
    - Estruturação do sistema de rotas inicial com `react-router-dom`,
      definindo a página de Login como a rota raiz (`/`).
    
    - Elaboração e atualização completa da documentação (`README.md`) para
      refletir a arquitetura do projeto e as tecnologias escolhidas.

--- Projeto-Avaliativo/recicla-365/src/App.jsx
+++ Projeto-Avaliativo/recicla-365/src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/LoginPage/Login.jsx";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Recicla 365</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
---
+++ Projeto-Avaliativo/recicla-365/src/components/atoms/button/Button.jsx
@@ -0,0 +1,10 @@
+import styles from './Button.module.css';
+
+const Button =({ children, onClick, type = 'button'}) => {
+    return(
+        <button className={styles.button} onClick={onClick} type={type}>
+            {children}
+        </button>
+    )
+}
+export default Button;
