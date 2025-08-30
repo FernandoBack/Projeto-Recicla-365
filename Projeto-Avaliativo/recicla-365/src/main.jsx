0bff7af9a17e342d94fc36dded7268a5ead6f187
FernandoBack <fernandoback@gmail.com>
2025-08-30 11:19:50 -0300

    feat: Refaruando pagina de login e criando Mok de usuário.
    -Criar Dados Falsos (Mocks): Para que a validação funcione, precisamos ter uma lista de usuários cadastrados no localStorage.
    
    -Refatorar a UI: Remover o link "Esqueceu a senha?" e trocar o link de cadastro por um botão.
    
    -Adicionar Estado (useState): Para controlar os valores dos campos de e-mail e senha.
    
    -Implementar a Função de Login (handleSubmit): Que será executada ao clicar em "Entrar", lendo o localStorage e validando os dados.

--- Projeto-Avaliativo/recicla-365/src/main.jsx
+++ Projeto-Avaliativo/recicla-365/src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { users } from './services/mockUsers.js'
+
+if (!localStorage.getItem('users')) {
+  localStorage.setItem('users', JSON.stringify(users))
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
