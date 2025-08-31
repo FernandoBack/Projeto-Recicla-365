5525e7840ee2f40ea31351ccf307140d2ed7ad21
FernandoBack <fernandoback@gmail.com>
2025-08-31 18:42:41 -0300

    fix: logo

--- Projeto-Avaliativo/recicla-365/src/components/organisms/Header/Header.jsx
+++ Projeto-Avaliativo/recicla-365/src/components/organisms/Header/Header.jsx
@@ -1,16 +1,11 @@
-// src/components/organisms/Header/Header.jsx
-
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../../../contexts/AuthContext';
-import styles from './Header.module.css';
 
 const Header = () => {
     const navigate = useNavigate();
-
     const location = useLocation();
     const { user, logout } = useAuth();
-    const location = useLocation();
 
     const handleLogout = () => {
         logout();
@@ -21,45 +16,32 @@ const Header = () => {
         <AppBar position="static">
             <Toolbar>
 
-                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                    <img
-                        src="/public/logo.png"
-                        style={{ height: '40px', cursor: 'pointer' }}
-                        onClick={() => navigate('/')}
-                    />
-                </Box>
-
-                {/* Logo para telas pequenas (se desejar uma versão menor ou diferente) */}
-                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                <Box sx={{ flexGrow: 1, cursor: 'pointer' }} onClick={() => navigate('/')}>
                     <img
                         src="/public/logo.png"
                         alt="Recicla365 Logo"
-                        style={{ height: '30px', cursor: 'pointer' }}
-                        onClick={() => navigate('/')}
+                        style={{ height: '40px', display: 'block' }}
                     />
                 </Box>
 
-                {user ? (
-                    <Box sx={{ display: 'flex' }}>
-                        {location.pathname !== '/dashboard' && (
-                            <Button color="inherit" onClick={() => navigate('/dashboard')}>
-                                Gerenciar Locais de Coleta
-                            </Button>
-                        )}
-                        <Button color="inherit" onClick={handleLogout}>
-                            Sair
-                        </Button>
-                    </Box>
-                ) : (
-                    <Box sx={{ display: 'flex' }}>
-                        <Button color="inherit" onClick={() => navigate('/cadastro')}>
-                            Cadastro
-                        </Button>
-                        <Button color="inherit" onClick={() => navigate('/login')}>
-                            Login
-                        </Button>
-                    </Box>
-                )}
+                <Box>
+                    {user ? (
+
+                        <>
+                            {location.pathname !== '/dashboard' && (
+                                <Button color="inherit" onClick={() => navigate('/dashboard')}>Gerenciar Pontos de Coleta</Button>
+                            )}
+
+                            <Button color="inherit" onClick={handleLogout}>Sair</Button>
+                        </>
+                    ) : (
+
+                        <>
+                            <Button color="inherit" onClick={() => navigate('/login')}>Entrar</Button>
+                            <Button color="inherit" onClick={() => navigate('/cadastro')}>Cadastre-se</Button>
+                        </>
+                    )}
+                </Box>
             </Toolbar>
         </AppBar>
     );
