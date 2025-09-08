28d2010be88f418dc9d8d14646da5e89353d24fd
FernandoBack <fernandoback@gmail.com>
2025-09-08 16:14:37 -0300

    fix: Att

--- Projeto-Avaliativo/recicla-365/src/components/organisms/Header/Header.jsx
+++ Projeto-Avaliativo/recicla-365/src/components/organisms/Header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
 
                 <Box sx={{ flexGrow: 1, cursor: 'pointer' }} onClick={() => navigate('/')}>
                     <img
-                        src="/public/logo-1.png"
+                        src="/src/public/logo-1.png"
                         alt="Recicla365 Logo"
                         style={{ height: '40px', display: 'block' }}
                     />
